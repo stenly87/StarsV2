056a29ec3fbc25494376a04d14051deb72ed0da7
Alex Pushkin <ifc@suz-ppk.ru>
2022-10-12 08:39:45 +1000

    Добавьте файлы проекта.

---
+++ StarsV2/GameMenu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StarsV2
+{
+    /// <summary>
+    /// Логика взаимодействия для GameMenu.xaml
+    /// </summary>
+    public partial class GameMenu : Window
+    {
+        SoundPlayer ready = new SoundPlayer(Environment.CurrentDirectory + "/Music/ready.wav");
+        public GameMenu()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Skins skins = new Skins();
+            ready.Play();
+            skins.Show();
+            Close();
+        }
+
+        
+    }
+}
---
+++ StarsV2/Skins.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StarsV2
+{
+    /// <summary>
+    /// Логика взаимодействия для Skins.xaml
+    /// </summary>
+    public partial class Skins : Window
+    {
+        SoundPlayer ready = new SoundPlayer(Environment.CurrentDirectory + "/Music/ready.wav");
+        public Skins()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GameMenu gameMenu = new GameMenu();
+            ready.Play();
+            gameMenu.Show();
+            Close();
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Game game = new Game();
+            
+            game.Show();
+            Close();
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Game2 game2 = new Game2();
+            
+            game2.Show();
+            Close();
+            
+        }
+    }
+}
